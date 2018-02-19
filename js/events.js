dc4407091d989e0c7b10b6151df2508d2d2eebc4
Aladdin Magdy <aladdinmagdysaber167@commerce.helwan.edu.eg>
2018-02-19 22:06:53 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('img').on('keydown',function(key){
+  $('document').on('keydown',function(key){
     if(key.which == 71){
       alert('you pressed "g"!!')
     }
